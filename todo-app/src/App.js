11618a4efd145376b4203f197940b5d0e9a55348
Pratima <gaitrisanu2468@gmail.com>
2022-05-10 11:26:15 +0545

    main route page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PageTitle from './components/PageTitle';
+
+function App() {
+  return (
+    <div className='button'>
+      <PageTitle>TODO LIST</PageTitle>
+    </div>
+  );
+}
+
+export default App;
