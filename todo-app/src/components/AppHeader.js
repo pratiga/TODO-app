895bff63e25e66e8684ff524b734eaf1f2a29279
Pratima <gaitrisanu2468@gmail.com>
2022-05-11 21:14:53 +0545

    option of select
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/AppHeader.js
+++ todo-app/src/components/AppHeader.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TodoModal from './TodoModal'
+import Button, { SelectButton } from './Button';
+
 
 function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <div>AppHeader</div>
+    <div>
+    <h1>hello from header</h1>
+    <Button  variant="primary" onClick={() =>
+     setModalOpen(true)}>
+    Add Task
+    </Button>
+    <SelectButton id="status">
+      <option value="all">All</option>
+      <option value="incomplete"> incomplete</option>
+      <option value="complete">complete</option>
+    </SelectButton>
+    <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+   
+    </div>
   )
 }
 
