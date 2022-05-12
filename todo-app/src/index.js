f2661826b8029d65d1f68f7d9000534cc94efb20
Pratima <gaitrisanu2468@gmail.com>
2022-05-12 21:16:34 +0545

    add store reducer
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './styles/GlobalStyles.css'
@@ -8,11 +10,14 @@ import'@fontsource/poppins/500.css';
 import'@fontsource/poppins/600.css';
 import'@fontsource/poppins/700.css';
 import reportWebVitals from './reportWebVitals';
-
+import { store } from './app/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
+   
   </React.StrictMode>
 );
 
