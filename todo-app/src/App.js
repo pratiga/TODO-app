5298ff8efa90080e436eb3e4daa3aed09e69d0f7
Pratima <gaitrisanu2468@gmail.com>
2022-05-13 21:30:05 +0545

    add toaster
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,15 +3,30 @@ import React from 'react';
 import PageTitle from './components/PageTitle';
 import style from './styles/modules/app.module.scss';
 import AppHeader from './components/AppHeader';
+// import AppContent from './components/AppContent';
+import { Toaster } from 'react-hot-toast'; 
 
 function App() {
   return (
+    <>
     <div className='container'>
       <PageTitle>TODO LIST</PageTitle>
       <div className={style.app__wrapper}>
-      <AppHeader></AppHeader>
+      <AppHeader />
+      {/* <AppContent></AppContent> */}
       </div>
     </div>
+    <Toaster
+    position = "buttom-right"
+    ToastOptions = {{
+      style: {
+        fontSize: '1.4rem',
+      }
+      
+    }}
+    >
+    </Toaster>
+    </>
   );
 }
 
