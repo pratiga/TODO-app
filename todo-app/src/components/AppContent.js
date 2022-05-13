d2386f9929ef6fde819164a06af955b5dee47db6
Pratima <gaitrisanu2468@gmail.com>
2022-05-13 22:34:33 +0545

    read all todo item
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ todo-app/src/components/AppContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useSelector } from 'react-redux';
+
+function AppContent() {
+    const todoList = useSelector((state) => state.todo.todoList);
+    const sortedTodoList = [...todoList]
+    sortedTodoList.sort((a,b) => new Date(b.time) - new Date(a.time))
+  return (
+    <div>
+    {sortedTodoList && sortedTodoList.length > 0
+     ? sortedTodoList.map((todo) => todo.title)
+     : 'no todo found'}
+    </div>
+  )
+}
+
+export default AppContent
