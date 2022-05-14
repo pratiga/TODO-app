1389d4756bb3f003f6d05cc7174e09b85d93b84b
Pratima <gaitrisanu2468@gmail.com>
2022-05-14 21:43:09 +0545

    add delete function
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/Todoitem.js
+++ todo-app/src/components/Todoitem.js
@@ -1,14 +1,62 @@
 import React from 'react'
-import styles from '../styles/modules/todoItems.module.scss'
+import styles from '../styles/modules/todoitem.module.scss'
+import { getClasses} from '../utils/getClasses';
+import {MdDelete,MdEdit} from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+//import { dispatch } from 'react-hot-toast/dist/core/store';
+import { deleteTodo } from '../slice/todoSlice';
+import toast from 'react-hot-toast';
 
 function Todoitem({ todo }) {
+  
+  const dispatch = useDispatch();
+  const handleUpdate = () => {
+      console.log('updating');
+  }
+    const handleDelete = () => {
+      dispatch(deleteTodo(todo.id));
+      toast.success('Todo deleted successfully');
+    };
+  
   return (
     <div className='styles.item'>
-    <div className={styles.todoDEtails}>
+    <div className={styles.todoDetails}>
     []
     <div className={styles.texts}>
-    <p>{ todo.title}</p>
+    <p
+    className={getClasses([
+      styles.todoText,
+      todo.status === 'complete' && styles
+      ['todoText--completed'],
+    ])}
+    >
+      {todo.title}
+    </p>
+    <p className={styles.time}>
+      {FormData(new Date(todo.time), 'p, MM/dd/yyyy')}
+    </p>
     </div>
+    </div>
+    
+    <div className={styles.todoActions}>
+      <div 
+      className={styles.icon}
+      onClick={handleDelete}
+      onKeyDown={handleDelete}
+      role="button"
+      tabIndex={0}
+      >
+      <MdDelete />
+      </div>
+      <div 
+      className={styles.icons}
+      onClick={handleUpdate}
+      onKeyDown={handleUpdate}
+      role="button"
+      tabIndex={0}
+      >
+        <MdEdit />
+      </div>
     </div>
     
     </div>
