c85ea0eead2fcd2fc6578cf45dc3e610f6547801
Pratima <gaitrisanu2468@gmail.com>
2022-05-15 22:00:05 +0545

    remove unrequired content
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/AppHeader.js
+++ todo-app/src/components/AppHeader.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import { useDispatch } from 'react-redux';
 import TodoModal from './TodoModal'
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss'
@@ -6,22 +7,25 @@ import styles from '../styles/modules/app.module.scss'
 
 function AppHeader() {
   const [modalOpen, setModalOpen] = useState(true);
+  // const dispatch = useDispatch()
+ 
   return (
     <div className={styles.appHeader}>
-    
-    <Button type='submit' variant="primary" onClick={() =>
-     setModalOpen(true)}>
-    Add Task
-    </Button>
-    <SelectButton id="status">
-      <option value="all">All</option>
-      <option value="incomplete"> incomplete</option>
-      <option value="complete">complete</option>
-    </SelectButton>
-    <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
-   
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
+      <SelectButton
+        id="status"
+        //onChange={(e) => updateFilter(e)}
+       // value={filterStatus}
+      >
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Completed</option>
+      </SelectButton>
+      <TodoModal type="add" modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
-  )
+  );
 }
 
-export default AppHeader
+export default AppHeader;
