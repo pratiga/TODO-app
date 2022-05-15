830131df0d1ecd1d92545d875d0319238a159552
Pratima <gaitrisanu2468@gmail.com>
2022-05-15 22:00:41 +0545

    add update function
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/Todoitem.js
+++ todo-app/src/components/Todoitem.js
@@ -1,27 +1,33 @@
+import {format} from 'date-fns/esm';
 import React from 'react'
 import styles from '../styles/modules/todoitem.module.scss'
 import { getClasses} from '../utils/getClasses';
 import {MdDelete,MdEdit} from 'react-icons/md';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 //import { dispatch } from 'react-hot-toast/dist/core/store';
 import { deleteTodo } from '../slice/todoSlice';
 import toast from 'react-hot-toast';
+import TodoModal from './TodoModal';
 
 function Todoitem({ todo }) {
   
   const dispatch = useDispatch();
-  const handleUpdate = () => {
-      console.log('updating');
-  }
+  // const [checked, setChecked] = useState(false);
+  const [updateModalOpen, setUpdateModalOpen] = useState(false)
+ 
     const handleDelete = () => {
       dispatch(deleteTodo(todo.id));
       toast.success('Todo deleted successfully');
     };
-  
+    const handleUpdate = () => {
+      setUpdateModalOpen(true)
+  }
   return (
+    <>
     <div className='styles.item'>
     <div className={styles.todoDetails}>
-    []
+    [ ]
     <div className={styles.texts}>
     <p
     className={getClasses([
@@ -33,7 +39,7 @@ function Todoitem({ todo }) {
       {todo.title}
     </p>
     <p className={styles.time}>
-      {FormData(new Date(todo.time), 'p, MM/dd/yyyy')}
+      {format(new Date(todo.time), 'p, MM/dd/yyyy')}
     </p>
     </div>
     </div>
@@ -41,17 +47,17 @@ function Todoitem({ todo }) {
     <div className={styles.todoActions}>
       <div 
       className={styles.icon}
-      onClick={handleDelete}
-      onKeyDown={handleDelete}
+      onClick={() => handleDelete()}
+      onKeyDown={() => handleDelete()}
       role="button"
       tabIndex={0}
       >
       <MdDelete />
       </div>
       <div 
-      className={styles.icons}
-      onClick={handleUpdate}
-      onKeyDown={handleUpdate}
+      className={styles.icon}
+      onClick={() => handleUpdate()}
+      onKeyDown={() => handleUpdate()}
       role="button"
       tabIndex={0}
       >
@@ -60,6 +66,13 @@ function Todoitem({ todo }) {
     </div>
     
     </div>
+    <TodoModal
+      type="update"
+      modalOpen = {updateModalOpen}
+      setModalOpen = {setUpdateModalOpen}
+      todo = {todo}
+    />
+    </>
   )
 }
 
