1c4dd40944e71e43248909bc09a8d4613bbcafd6
Pratima <gaitrisanu2468@gmail.com>
2022-05-16 20:57:58 +0545

    solve error
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/AppHeader.js
+++ todo-app/src/components/AppHeader.js
@@ -6,14 +6,25 @@ import styles from '../styles/modules/app.module.scss'
 
 
 function AppHeader() {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
   // const dispatch = useDispatch()
+  const handleUpdate = () => {
+    setModalOpen(true)
+}
  
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary" onClick={() => setModalOpen(true)}>
-        Add Task
+      <Button>
+      <div 
+      onClick={() => handleUpdate()}
+      onKeyDown={() => handleUpdate()}
+      
+      >
+         Add Task
+      </div>
       </Button>
+      
+      
       <SelectButton
         id="status"
         //onChange={(e) => updateFilter(e)}
