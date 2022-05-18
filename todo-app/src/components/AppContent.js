72acfcee1bc25db87c82783246a45b7c0b50f529
Pratima <gaitrisanu2468@gmail.com>
2022-05-18 20:58:32 +0545

    filter status
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- todo-app/src/components/AppContent.js
+++ todo-app/src/components/AppContent.js
@@ -1,20 +1,61 @@
 import React from 'react'
 import {useSelector } from 'react-redux';
 import Todoitem from './Todoitem';
+import { motion } from 'framer-motion';
+import styles from '../styles/modules/app.module.scss';
+
+const container = {
+  hidden: { opacity : 1 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const child = {
+  hidden: { y: 20, opacity: 0},
+  visible: {
+    y:0,
+    opacity: 1,
+    
+  },
+};
 
 function AppContent() {
     const todoList = useSelector((state) => state.todo.todoList);
+    const filterStatus = useSelector((state)=> state.todo.filterStatus);
     const sortedTodoList = [...todoList]
     sortedTodoList.sort((a,b) => new Date(b.time) - new Date(a.time))
+    
+    const filteredTodoList = sortedTodoList.filter((item) => {
+      if (filterStatus === 'all') {
+        return true;
+      }
+      return item.status === filterStatus;
+    });
+    
   return (
-    <div>
-    {sortedTodoList && sortedTodoList.length > 0
-     ? (sortedTodoList.map((todo) => (
-       <Todoitem key={todo.id} todo={todo} />
-       ))
-    ) : ( 'no todo found')}
-    </div>
-  )
+    <motion.div 
+      className={styles.content__wrapper}
+      variants={container}
+      initial="hidden"
+      animate= "visible"
+      >
+    {filteredTodoList && filteredTodoList.length > 0
+     ? (filteredTodoList.map((todo) => 
+       <Todoitem key={todo.id} todo={todo} />)
+        ) : (
+          <motion.p className={styles.emptyText}
+          variants = {child}>
+          No todo found
+          </motion.p>
+             
+             )}
+    </motion.div>
+  );
 }
 
-export default AppContent
+export default AppContent;
